d1ee300185e116b2e23c0c18892b1efee37b9c78
Takumo <leomanuelisla@gmail.com>
2026-03-17 21:32:00 +0100

    Implement counter functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Guardamos el número en una variable
+let contador = 0;
 
+// Buscamos los elementos del HTML
+let texto = document.getElementById("contador");
+let boton = document.getElementById("boton");
+
+// Cuando haces click en el botón
+boton.onclick = function() {
+
+  // Sumamos 1
+  contador = contador + 1;
+
+  // Actualizamos el número en pantalla
+  texto.textContent = contador;
+};
